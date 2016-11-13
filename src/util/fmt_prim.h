28c71f7204147d3c7c3547295796ad8e796f5b89
iitalics <iitalics@gmail.com>
2016-11-13 13:25:27 -0500

    binary format

--- src/util/fmt_prim.h
+++ src/util/fmt_prim.h
@@ -14,8 +14,13 @@ namespace fmt {
   {
     long value;
     explicit Hex (long v)
-      : value(v)
-    {}
+      : value(v) {}
+  };
+  struct Bin
+  {
+    long value;
+    explicit Bin (long v)
+      : value(v) {}
   };
   
   template <typename Out>
@@ -23,6 +28,13 @@ namespace fmt {
   {
     char tmp[24];
     size_t len = int_to_str(tmp, x.value, 16);
+    out.write(Slice<const char>(tmp, len));
+  }
+  template <typename Out>
+  void writer (Out& out, Bin x)
+  {
+    char tmp[24];
+    size_t len = int_to_str(tmp, x.value, 2);
     out.write(Slice<const char>(tmp, len));
   }
 
