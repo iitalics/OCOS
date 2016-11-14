dda84b1806f45cd8581f9371214ec4ed1f1453e6
iitalics <iitalics@gmail.com>
2016-11-14 00:41:30 -0500

    rudimentary mmap scanning

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -3,6 +3,17 @@
 #include <util/fmt_prim.h>
 #include <util/fmt.h>
 
+/* fmt::hex() is having issues with unsigned ints */
+static void hex_addr (io::VGA& vga, ulong addr)
+{
+  char buf[8];
+  for (int i = 0; i < 8; i++) {
+    buf[7 - i] = "0123456789abcdef"[addr & 0xf];
+    addr = addr >> 4;
+  }
+  vga.write(String(buf, 8));
+}
+
 extern "C" {
   void kernel_main (void)
   {
@@ -19,10 +30,42 @@ extern "C" {
     fmt::format(vga,
                 "multiboot info:\n"
                 "  flags:     ~a\n"
-                "  mem-lower: ~a KB\n"
-                "  mem-upper: ~a KB\n",
-                fmt::pad(12, fmt::bin(mbi.flags)),
-                fmt::pad(12, mbi.mem_lower),
-                fmt::pad(12, mbi.mem_upper));
+                "  mem-lower: ~a kB  mem-upper: ~a kB\n",
+                fmt::bin(mbi.flags),
+                fmt::pad(6, mbi.mem_lower),
+                fmt::pad(6, mbi.mem_upper));
+
+    using ulong = unsigned long;
+    ulong mmap = mbi.mmap_addr;
+
+    while (mmap < mbi.mmap_addr + mbi.mmap_length) {
+      /* thanks, multiboot standard :] */
+      mmap += sizeof(ulong);
+      ulong* fields = (ulong*) mmap;
+      ulong size    = fields[-1];
+      ulong base_lo = fields[0];
+      ulong base_hi = fields[1];
+      ulong len_lo  = fields[2];
+      ulong len_hi  = fields[3];
+      ulong type    = fields[4];
+      mmap += size;
+
+      if (type == 1)
+        vga.set_style(io::style_of(io::LightCyan));
+      else
+        vga.set_style(io::style_of(io::DarkGrey));
+
+      fmt::format(vga,
+                  "entry type: ~a ~a\n"
+                  "     length: ~a kB\n"
+                  "  base addr: ",
+                  type,
+                  type == 1 ? string("(usable)") : string("**reserved**"),
+                  len_lo >> 10);
+      hex_addr(vga, base_hi);
+      vga.write(' ');
+      hex_addr(vga, base_lo);
+      vga.line_break();
+    }
   }
 };
