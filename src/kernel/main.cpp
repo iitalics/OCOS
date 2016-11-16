e0cafff3f4b483baa488ed1a82d5c202536dfaeb
iitalics <iitalics@gmail.com>
2016-11-16 10:52:10 -0500

    kernel endpoints (_kernel_begin, _kernel_end)

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -4,11 +4,25 @@
 #include <util/fmt_prim.h>
 #include <util/fmt.h>
 
-extern "C" {  
+extern "C" {
+  extern char _kernel_begin, _kernel_end;
+
   void kernel_main (void)
   {
     io::VGA vga;
     vga.clear();
+
+    auto k_begin = &_kernel_begin;
+    auto k_end = &_kernel_end;
+    unsigned long k_size = (unsigned long) k_end - (unsigned long) k_begin;
+    fmt::format(vga,
+                "located kernel:\n"
+                "~a - ~a  (~a bytes)\n"
+                "kernel_main()  @  ~a\n"
+                "sp  @  ~a\n",
+                k_begin, k_end, k_size,
+                kernel_main,
+                &vga);
 
     if (multiboot_info_ptr != nullptr) {
       mem::find_ram_multiboot(*multiboot_info_ptr);
@@ -19,9 +33,13 @@ extern "C" {
                 ram.len());
 
     for (auto region : ram) {
-      fmt::format(vga, "  ~a: ~a kB\n",
+      fmt::format(vga, "  ~a: ~a kB / ~a mB\n",
                   region.data(),
-                  region.len() >> 10);
+                  region.len() >> 10,
+                  region.len() >> 20);
     }
+
+    vga.line_break();
+    vga.line_break();
   }
 };
