95a3596c4458ad18c2105da52f2eae3082c083fd
iitalics <iitalics@gmail.com>
2016-11-16 15:56:58 -0500

    mem::find_ram_multiboot wary of kernel code

--- src/kernel/mem.cpp
+++ src/kernel/mem.cpp
@@ -1,13 +1,38 @@
 #include "mem.h"
+extern "C" {
+  extern char _kernel_begin, _kernel_end;
+};
 namespace mem {
+  static Region validate_ram_region (unsigned long addr, unsigned long len)
+  {
+    /* regions in kernel code are not valid! */
+    if (addr >= (unsigned long) &_kernel_begin
+        && addr < (unsigned long) &_kernel_end) {
+      /* adjust to latest page after kernel code */
+      auto new_addr = (unsigned long) &_kernel_end;
+      if (new_addr & 0xfff)
+        new_addr = (new_addr | 0xfff) + 1;
+      /* no bytes left? */
+      if (new_addr > addr + len)
+        len = 0;
+      else
+        len = (addr + len) - new_addr;
+      addr = new_addr;
+    }
+    return Region((char*) addr, len);
+  }
+
   /* RAM regions */
   const size_t max_ram_regions = 16;
   static size_t n_ram_regions = 0;
   static Region ram_regions[max_ram_regions];
   static size_t add_ram_region (unsigned long addr, unsigned long len)
   {
-    if (n_ram_regions < max_ram_regions) {
-      ram_regions[n_ram_regions++] = Region((char*) addr, len);
+    /* validate */
+    auto reg = validate_ram_region(addr, len);
+    /* don't overflow the buffer */
+    if (reg.len() > 0 && n_ram_regions < max_ram_regions) {
+      ram_regions[n_ram_regions++] = reg;
       return 1;
     }
     else {
@@ -23,10 +48,11 @@ namespace mem {
     using ulong = unsigned long;
     size_t count = 0;
 
+    /* flag[6] => memory map available */
     if (mbi.flags & (1 << 6)) {
       ulong mmap = mbi.mmap_addr;
       while (mmap < mbi.mmap_addr + mbi.mmap_length) {
-        /* thanks, multiboot standard :] */
+        /* read fields from multiboot struct */
         mmap += sizeof(ulong);
         ulong* fields = (ulong*) mmap;
         ulong size    = fields[-1];
